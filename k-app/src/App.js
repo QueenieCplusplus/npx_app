f414e4bfe67b6c43238fc3cf95d0eaf22893a9ea
QueenieCplusplus <queeniecplusplus@gmail.com>
2020-06-10 22:43:54 +0800

    0610 add moduleK using Promise to App.js

--- k-app/src/App.js
+++ k-app/src/App.js
@@ -1,26 +1,67 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Component } from 'react';
+//import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Karen's <code>src/App.js</code> ~
-        </p>
-        <a
-          className="App-link"
-          href="https://github.com/QueenieCplusplus?tab=repositories"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Karen's React App
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+
+  onClickListener = () => {
+
+    // import().then(
+
+    // ).catch()
+
+    import('./moduleK')
+      .then(
+
+      ({moduleK}) => {}
+      
+      ) /* using Promise*/
+      .catch(
+
+        err => {}
+
+      );
+
+  };
+
+  render(){
+
+    return(
+      <div>
+
+        <button onClick ={this.onClickListener}>
+           click on me plz
+        </button>
+
+      </div>
+
+    );
+
+  }
+
 }
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Karen's <code>src/App.js</code> ~
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://github.com/QueenieCplusplus?tab=repositories"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Karen's React App
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
---
+++ k-app/src/moduleK.js
@@ -0,0 +1,5 @@
+//0610 add module by Karen 
+
+const moduleK = 'Hi this is Karen';
+
+export {moduleK};
