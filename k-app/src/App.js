49923c3947e3040fc839328529d1e3662f57ad5e
QueenieCplusplus <queeniecplusplus@gmail.com>
2020-06-10 22:07:29 +0800

    06/10 edit App.js & logo.svg under SRC

--- k-app/src/App.js
+++ k-app/src/App.js
@@ -8,15 +8,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Karen's <code>src/App.js</code> ~
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/QueenieCplusplus?tab=repositories"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Karen's React App
         </a>
       </header>
     </div>
